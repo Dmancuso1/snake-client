956857d46843fefe154784f0ba1f4538aa4aec9d
Dane Mancuso <danegmancuso@gmail.com>
2020-07-01 17:09:49 +0000

    added exports of client main fucntion. added connect event to write name to server

---
+++ client.js
@@ -0,0 +1,43 @@
+const net = require('net');
+
+
+
+
+/*
+Establishes connection with the game server
+*/
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incomming data as text.
+  conn.setEncoding('utf8');
+
+  // sends message back to the server.
+  conn.on('data', () => {
+  console.log('you ded cuz you idled');
+  });
+
+  conn.on('connect', () => {
+  console.log('Successfully connected to game server');
+  });
+
+  conn.on('connect', () => {
+  conn.write('Name: DGM');
+  });
+
+  return conn;
+
+}
+
+
+
+
+
+// adding the 'connect' function to object. 
+const connectObj = {
+  connect,
+}
+
+module.exports = connectObj;
--- play.js
+++ play.js
@@ -1,24 +1,3 @@
-const net = require('net');
-
-/*
-Establishes connection with the game server
-*/
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incomming data as text.
-  conn.setEncoding('utf8');
-
-  // sends message back to the server.
-  conn.on('data', () => {
-  console.log('you ded cuz you idled');
-  });
-
-  return conn;
-
-}
-
-console.log('connecting...');
+const { connect } = require('./client');
+console.log('Connecting ...');
 connect();
